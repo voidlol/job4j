17f90b2237717764afe6382188811ab064764f40
voidlol <udroguedozed@gmail.com>
2018-11-26 22:55:46 +0300

    3.4 добавил джавадок

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -15,6 +15,11 @@ public class Point {
         this.y = y;
     }
 
+    /**
+     * Вычисляет расстояние между точками в системе координат
+     * @param that - точка, расстояние до которой необходимо вычислить
+     * @return - расстояние
+     */
     public double distanceTo(Point that) {
         return Math.sqrt(
                 Math.pow(that.x - this.x, 2) + Math.pow(that.y - this.y, 2));
