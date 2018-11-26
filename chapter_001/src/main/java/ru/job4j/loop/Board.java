4880ccd7c6a10c7db825c2fb5eac68fda62f995e
voidlol <udroguedozed@gmail.com>
2018-11-26 18:11:31 +0300

    5.3 Псевдографика

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,28 @@
+package ru.job4j.loop;
+
+public class Board {
+    /**
+     * Рисуем доску по условию:
+     * Если сумма координат является четным числом - рисуем X, в противном случае - ничего.
+     *
+     * @param wight - ширина доски
+     * @param height - высота доски
+     * @return - доска
+     */
+    public String paint(int wight, int height) {
+        StringBuilder result = new StringBuilder();
+        String ln = System.lineSeparator();
+
+        for (int i = 0; i < wight; i++) {
+            for (int j = 0; j < height; j++) {
+                if ((i + j) % 2 == 0) {
+                    result.append("X");
+                } else
+                    result.append(" ");
+            }
+            result.append(ln);
+        }
+
+        return result.toString();
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BoardTest {
+    @Test
+    public void board3x3() {
+        Board board = new Board();
+        String ln = System.lineSeparator();
+        assertThat(board.paint(3,3), is(String.format("X X%s X %sX X%s", ln, ln, ln)));
+    }
+
+    @Test
+    public void board5x5() {
+        Board board = new Board();
+        String ln = System.lineSeparator();
+        assertThat(board.paint(5,5), is(String.format("X X X%s X X %sX X X%s X X %sX X X%s", ln, ln, ln, ln, ln)));
+    }
+}
