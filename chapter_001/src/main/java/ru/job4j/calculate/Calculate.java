aa7b4402d36d34fc8e1c351d589817dfb0128b0b
voidlol <udroguedozed@gmail.com>
2018-11-26 14:28:12 +0300

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,18 +1,27 @@
 package ru.job4j.calculate;
 
-/*
+/**
 * Calculate
 *
 * @author Andrey Savelov (udroguedozed@gmail.com)
 */
 public class Calculate {
 
-	/*
+	/**
 	* Main
 	* @param args - args
 	*/
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
+	}
+	
+	/**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+	public String echo(String name) {
+		return "Echo, echo, echo: " + name;
 	}
 }
 
---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Calculate Test
+*
+* @author Andrey Savelov (udroguedozed@gmail.com)
+*/
+public class CalculateTest {
+	/**
+	* Test echo.
+	*/
+	@Test
+	public void whenTakeNameThenTreeEchoPlusName() {
+		String input = "Andrey S";
+		String expect = "Echo, echo, echo: Andrey S"; 
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	}
+}
+
