ff24eeb8b937d69a74d238f9a641faa815bab751
voidlol <udroguedozed@gmail.com>
2018-11-28 22:03:38 +0300

    Принципы ООП: Реализация профессий в коде

--- chapter_002/src/main/java/ru/job4j/professions/Doctor.java
+++ chapter_002/src/main/java/ru/job4j/professions/Doctor.java
@@ -2,6 +2,6 @@ package ru.job4j.professions;
 
 public class Doctor extends Profession {
     public Diagnose heal(Patient patient) {
-
+        return new Diagnose();
     }
 }
--- chapter_002/src/main/java/ru/job4j/professions/Profession.java
+++ chapter_002/src/main/java/ru/job4j/professions/Profession.java
@@ -5,6 +5,6 @@ public class Profession {
     private String profession;
 
     public String getName() {
-
+        return this.name;
     }
 }
