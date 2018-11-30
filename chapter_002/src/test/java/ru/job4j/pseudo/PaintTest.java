ba0b81761e64a2ca2affc831a29af3c01fff44c3
voidlol <udroguedozed@gmail.com>
2018-11-30 15:17:06 +0300

    Принципы ООП: Рефакторинг

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,7 +1,8 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
@@ -10,21 +11,28 @@ import static org.junit.Assert.*;
 
 public class PaintTest {
 
+    private final PrintStream STDOUT = System.out;
+    private final ByteArrayOutputStream OUT = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.OUT));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.STDOUT);
+    }
+
     @Test
     public void drawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
-        assertThat(out.toString(), is(new StringBuilder().append("####").append("####").append("####").append("####").append(System.lineSeparator()).toString()));
+        assertThat(this.OUT.toString(), is(new StringBuilder().append("####").append("####").append("####").append("####").append(System.lineSeparator()).toString()));
     }
 
     @Test
     public void drawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
-        assertThat(out.toString(), is(new StringBuilder().append("   #   ").append("  ###  ").append(" ##### ").append("#######").append(System.lineSeparator()).toString()));
+        assertThat(this.OUT.toString(), is(new StringBuilder().append("   #   ").append("  ###  ").append(" ##### ").append("#######").append(System.lineSeparator()).toString()));
     }
 }
