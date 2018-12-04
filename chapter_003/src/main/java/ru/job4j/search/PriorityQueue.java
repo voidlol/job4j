ecab047094045e621c7271fd266cc4e9bce62ef0
voidlol <udroguedozed@gmail.com>
2018-12-04 16:40:36 +0300

    Collections Lite: Вставка в linkedlist

---
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,28 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /**
+     * Метод должен вставлять в нужную позицию элемент.
+     * Позиция определять по полю приоритет.
+     * Для вставик использовать add(int index, E value)
+     * @param task задача
+     */
+    public void put(Task task) {
+        int index = 0;
+        for (Task t : tasks) {
+            if (task.getPriority() < t.getPriority()) {
+                break;
+            }
+            index++;
+        }
+        tasks.add(index, task);
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
