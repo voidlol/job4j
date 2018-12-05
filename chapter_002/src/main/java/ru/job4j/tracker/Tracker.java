2c7a11e8a694f38e5d73668d1f5a8754f5e49f6f
voidlol <udroguedozed@gmail.com>
2018-12-05 15:36:04 +0300

    Changes to tracker

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -32,12 +32,11 @@ public class Tracker {
      */
     public boolean replace(String id, Item item) {
         boolean result = false;
-        for (Item i : items) {
-            if (i.getId().equals(id)) {
-                items.remove(i);
-                item.setId(i.getId());
-                items.add(item);
+        for (int i = 0; i < this.items.size(); i++) {
+            if (this.items.get(i).getId().equals(id)) {
                 result = true;
+                item.setId(this.items.get(i).getId());
+                this.items.set(i, item);
                 break;
             }
         }
@@ -50,8 +49,8 @@ public class Tracker {
      */
     public boolean delete(String id) {
         boolean result = false;
-        for (Item i : items) {
-            if (i.getId().equals(id)) {
+        for (int i = 0; i < this.items.size(); i++) {
+            if (this.items.get(i).getId().equals(id)) {
                 result = true;
                 this.items.remove(i);
                 break;
