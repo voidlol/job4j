85680f6c85aedd60b4d5773f1d89ede459421d83
voidlol <udroguedozed@gmail.com>
2018-12-20 15:41:41 +0300

    EvenIterator

---
+++ chapter_005/src/main/java/ru/job4j/iterator/EvenIterator.java
@@ -0,0 +1,37 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenIterator implements Iterator {
+
+    private final int[] values;
+    private int index = 0;
+
+    public EvenIterator(final int[] values) {
+        this.values = values;
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean rst = false;
+        for (int i = this.index; i < this.values.length; i++) {
+            if (this.values[i] % 2 == 0) {
+                rst = true;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    @Override
+    public Object next() {
+        while (this.values[this.index] % 2 != 0) {
+            this.index++;
+            if (this.index == this.values.length) {
+                throw new NoSuchElementException();
+            }
+        }
+        return this.values[this.index++];
+    }
+}
