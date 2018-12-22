fe7d97938f8cb0f0a4f4af57f4ddc676660e826d
voidlol <udroguedozed@gmail.com>
2018-12-22 21:55:27 +0300

    SimpleLinkedList with Iterator

--- chapter_005/src/main/java/ru/job4j/lists/SimpleLinkedList.java
+++ chapter_005/src/main/java/ru/job4j/lists/SimpleLinkedList.java
@@ -1,19 +1,25 @@
 package ru.job4j.lists;
 
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
  * @author - Andrey Savelov
  * @version - 1.0
  * @since - 22.12.2018
  */
-public class SimpleLinkedList<T> {
+public class SimpleLinkedList<T> implements Iterable {
     private int size;
     private Node<T> first;
+    private int modCount = 0;
 
     public void add(T element) {
         Node<T> newLink = new Node<>(element);
         newLink.next = this.first;
         this.first = newLink;
         this.size++;
+        this.modCount++;
     }
 
     public T get(int index) {
@@ -26,11 +32,39 @@ public class SimpleLinkedList<T> {
 
     public T delete() {
         this.first = this.first.next;
+        this.modCount++;
         return this.first.element;
     }
 
     public int getSize() {
         return this.size;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+
+            private Node<T> position = first;
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return this.position.next != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T rst = position.element;
+                position = position.next;
+                return rst;
+            }
+        };
     }
 
     private static class Node<T> {
