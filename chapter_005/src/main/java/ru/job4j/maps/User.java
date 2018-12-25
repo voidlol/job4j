0167b4960c05f8d6d8010e33066719707040f1c3
voidlol <udroguedozed@gmail.com>
2018-12-25 19:22:01 +0300

    Class User

---
+++ chapter_005/src/main/java/ru/job4j/maps/User.java
@@ -0,0 +1,14 @@
+package ru.job4j.maps;
+
+import java.util.Calendar;
+
+/**
+ * @author - Andrey Savelov
+ * @version - 1.0
+ * @since - 24.12.2018
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+}
