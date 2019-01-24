c045fa5f8cae30928f9ed845bf7fcfa87ec72406
voidlol <udroguedozed@gmail.com>
2019-01-24 13:05:23 +0300

    Остановка потребителя

---
+++ chapter_006/src/main/java/ru/job4j/concurrency/ParallelSearch.java
@@ -0,0 +1,41 @@
+package ru.job4j.concurrency;
+
+/**
+ * @author - Andrey Savelov
+ * @version - 1.0
+ * @since - 24.01.2019
+ */
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+
+        ).start();
+    }
+}
+
