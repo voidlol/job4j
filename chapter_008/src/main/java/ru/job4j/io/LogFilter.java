5fcbb9311de203c65b967d55cd103fbb59dac314
Voidlol <udroguedozed@gmail.com>
2021-05-19 19:54:14 +0300

    Log File Save filter

--- chapter_008/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_008/src/main/java/ru/job4j/io/LogFilter.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,8 +19,16 @@ public class LogFilter {
         }
         return result;
     }
+
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+            log.forEach(out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        System.out.println(log);
+        save(log, "404.txt");
     }
 }
